a1b3150d916701b03abbf46704888f922987e0ab
Alexander Burkov <a.p.burkov-at-gmail-dot-com>
2014-03-27 15:21:28 +0400

    init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,14 +1,10 @@
 ;; TODO
-;; * make tabs visible
-;; * configure `package'
+;; * make tabs and spaces visible
 ;; * line-move-viaible & EOL cursor sticking
 ;; * rainbow-delimiters: change colors to more contrast
 ;; * paredit: investigate how to use it
 ;; * expand-region: same as paredit
-;; * nrepl: same (see also seeder)
-;; * clojure mode
-;; * erlang mode
-;; * learn you some org-mode
+
 
 (custom-set-variables
  '(cursor-in-non-selected-windows nil)              ; don't display cursor in inactive window
@@ -17,7 +13,7 @@
  '(tool-bar-mode nil)                               ; don't display toolbar
  '(scroll-margin 8)                                 ; start scrolling 8 lines before end of a page
  '(scroll-step 1)                                   ; keyboard scroll step
- '(mouse-wheel-scroll-amount '(1 ((shift) . 1)))    ; one line at a time
+ '(mouse-wheel-scroll-amount '(4 ((shift) . 4)))    ; one line at a time
  '(mouse-wheel-progressive-speed nil)               ; don't accelerate scrolling
  '(mouse-wheel-follow-mouse t)                      ; scroll window under mouse
  '(initial-frame-alist '((fullscreen . maximized))) ; open emacs window maximized
@@ -27,15 +23,18 @@
  '(column-number-mode t)                            ; always show column numbers
  )
 
+;; (add-to-list 'load-path "/opt/erlang/lib/tools-2.6.13/emacs/")
+
 (server-start)                                      ; start in server mode, so you can use emacsclient 
 
 (defadvice split-window (after move-pointer-to-new-window activate) ; move focus to newly created window after split
   (other-window 1))
 
-(global-hl-line-mode 1)                                  ; enable current line highlighting
-(set-face-background hl-line-face "gray20")              ; and choose it color
-(load-file "~/.emacs.d/color-themes/themes/xoria256.el") ; load theme
-(color-theme-xoria256)                                   ; and apply it
+;; (global-hl-line-mode 1)                                  ; enable current line highlighting
+;; (set-face-background hl-line-face "gray20")              ; and choose it color
+;; (load-file "~/.emacs.d/color-themes/themes/xoria256.el") ; load theme
+;; (color-theme-xoria256)                                   ; and apply it
+(load-theme 'tango-dark t)
 
 (global-linum-mode)
 (savehist-mode)
@@ -55,4 +54,4 @@
 (package-initialize)
 
 ;; fonts
-(set-face-attribute 'default nil :height 110 :family "DejaVu Sans Mono")
+;; (set-face-attribute 'default nil :height 110 :family "DejaVu Sans Mono")
--- .zshrc
+++ .zshrc
@@ -2,29 +2,19 @@ ZSH=$HOME/.oh-my-zsh
 
 ZSH_THEME="candy"
 
-# Set to this to use case-sensitive completion
-# CASE_SENSITIVE="true"
-# Uncomment this to disable bi-weekly auto-update checks
-# DISABLE_AUTO_UPDATE="true"
-# Uncomment to change how often before auto-updates occur? (in days)
-# export UPDATE_ZSH_DAYS=13
-# Uncomment following line if you want to disable colors in ls
-# DISABLE_LS_COLORS="true"
-# Uncomment following line if you want to disable autosetting terminal title.
-# DISABLE_AUTO_TITLE="true"
-# Uncomment following line if you want to disable command autocorrection
-# DISABLE_CORRECTION="true"
-# Uncomment following line if you want red dots to be displayed while waiting for completion
-# COMPLETION_WAITING_DOTS="true"
-
-# Uncomment following line if you want to disable marking untracked files under
-# VCS as dirty. This makes repository status check for large repositories much,
-# much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-plugins=(git gitignore rebar bundler gem rake ruby rvm debian themes autojump colored-man history-substring-search extract vundle pass)
+plugins=(git gitignore rebar themes colored-man history-substring-search extract)
 
 source $ZSH/oh-my-zsh.sh
-export PATH=$PATH:/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:~/bin
 
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH=$PATH:/usr/lib/lightdm/lightdm
+export PATH=$PATH:/usr/local/sbin
+export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:/usr/sbin
+export PATH=$PATH:/usr/bin
+export PATH=$PATH:/sbin
+export PATH=$PATH:/bin
+export PATH=$PATH:~/bin
+
+export PATH=$PATH:/opt/erlang/bin
+export PATH=$PATH:/opt/idea/bin
+export PATH=$PATH:/opt/wireshark/bin
